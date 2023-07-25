0ff034a9dff26a4da8a4756e2dac966a8063d843
Daniel Esther <tunrayodaniels@gmail.com>
2023-07-25 14:30:45 -0700

    edited some files

--- functions1.c
+++ functions1.c
@@ -49,7 +49,7 @@ int print_octal(va_list types, char buffer[],
 	unsigned long int num = va_arg(types, unsigned long int);
 	unsigned long int init_num = num;
 
-	UNUSED(width)
+	UNUSED(width);
 		num = convert_size_unsgnd(num, size);
 	if (num == 0)
 		buffer[i--] = '0';
